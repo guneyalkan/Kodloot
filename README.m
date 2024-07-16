66eaa2d325bba84bdd4f5281635ce6c14f974fb2
Güney İsmail Alkan <143964425+guneyalkan@users.noreply.github.com>
2024-07-16 19:26:31 +0300

    Update README.m

--- README.m
+++ README.m
@@ -5,6 +5,6 @@
 - 📫 How to reach me .guneyismailalkan@gmail.com..
 
 <!---
-Kodloot/Kodloot is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+guneyalkan/guneyalkan is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
 --->
